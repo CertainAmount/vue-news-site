8c661d512518eb13ce3010967ee59c76bd0555a0
jeongil.yang <monday@kakao.com>
2020-12-19 03:04:06 +0900

    Call api using axios

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,12 +1,26 @@
 <template>
-<div>
-  news
-</div>
+  <div>
+    <div v-bind:key="user" v-for="user in users">{{ user.title }}</div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: "NewsView"
+  name: "NewsView",
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    let vm = this;
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+    .then(response =>
+        vm.users = response.data)
+    .catch(err => console.log(err))
+  }
 }
 </script>
 
