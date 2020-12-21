8522cbf840ffb9f2a60bf74d9b2e6468fa9245ae
jeongil.yang <monday@kakao.com>
2020-12-22 02:54:30 +0900

    Apply api using vuex

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,25 +1,15 @@
 <template>
   <div>
-    <div v-bind:key="user" v-for="user in users">{{ user.title }}</div>
+    <div v-bind:key="item" v-for="item in this.$store.state.news">{{ item.title }}</div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '@/api/index';
 
 export default {
   name: "NewsView",
-  data() {
-    return {
-      users: []
-    }
-  },
   created() {
-    fetchNewsList()
-        .then(response =>
-            this.users = response.data)
-        .catch(err =>
-            console.log(err))
+    this.$store.dispatch('FETCH_NEWS')
   }
 }
 </script>
