565e6facfdef179bc5e677d51360c32f674a0999
jeongil.yang <monday@kakao.com>
2020-12-25 22:08:16 +0900

    Change css of news, item view

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,7 +1,25 @@
 <template>
 <div>
-  <p>{{ fetchItem.title }}</p>
-  <div>{{ fetchItem.content }}</div>
+  <section>
+    <div class="user-container">
+      <div>
+        <i class="fas fa-user"></i>
+      </div>
+      <div class="user-description">
+        <router-link :to="'/user/' + fetchItem.user">
+          {{ fetchItem.user }}
+        </router-link>
+        <div class="time">
+          {{ fetchItem.time_ago }}
+        </div>
+      </div>
+    </div>
+    <h2>{{ fetchItem.title }}</h2>
+  </section>
+  <section>
+    <div v-html="fetchItem.content">
+    </div>
+  </section>
 </div>
 </template>
 
@@ -18,10 +36,23 @@ export default {
   created() {
     const itemId = this.$route.params.id
     this.$store.dispatch('FETCH_ITEM', itemId)
-  }
+  },
 }
 </script>
 
 <style scoped>
-
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.78rem;
+}
 </style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <p v-bind:key="item" v-for="item in fetchedNews">
-      <a v-bind:href="item.url">{{ item.title }}</a>
-      <small>
-        {{ item.time_ago }} by <router-link :to="'/user/'+item.user">{{ item.user }}</router-link>
-      </small>
-    </p>
+    <ul class="new-list">
+        <li class="post" v-bind:key="item" v-for="item in fetchedNews">
+          <div class="points">
+            {{ item.points }}
+          </div>
+          <div>
+            <p class="news-title">
+              <a v-bind:href="item.url">{{ item.title }}</a>
+            </p>
+            <small class="link-text">
+              {{ item.time_ago }} by
+              <router-link class="link-text" :to="'/user/'+item.user">{{ item.user }}</router-link>
+            </small>
+          </div>
+        </li>
+    </ul>
   </div>
 </template>
 
@@ -26,5 +36,28 @@ export default {
 </script>
 
 <style scoped>
-
+.new-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  height: 60px;
+  width: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
